7aa134d8be3050362da64f84e8ff557321b7ba80
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-02-01 09:37:24 -0800

    Branching Estimator Assignment

---
+++ BranchingEstimator/BranchingEstimator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BranchingEstimator
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Title and Welcome
+            Console.WriteLine("Welcome to Package Express. \nPlease follow the instructions below.");
+
+            //Package Weight and conversion
+            Console.WriteLine("Please enter the package weight (to nearest lbs):");
+            int packageweight = Convert.ToInt32(Console.ReadLine());
+
+            //Package Width and conversion
+            Console.WriteLine("Please enter the package width (to nearest inch):");
+            int packagewidth = Convert.ToInt32(Console.ReadLine());
+
+            //Package Height and conversion
+            Console.WriteLine("Please enter the package height (to nearest inch):");
+            int packageheight = Convert.ToInt32(Console.ReadLine());
+
+            //Package length and conversion
+            Console.WriteLine("Please enter the package length (to nearest inch):");
+            int packagelength = Convert.ToInt32(Console.ReadLine());
+
+            //Branch to see if package is too big
+            if (packageheight + packagewidth + packagelength > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            //If the package is not too big, it will calculate the estimate
+            else
+            {
+                decimal estimate = ((packageheight * packagewidth * packagelength * packageweight) / 100);
+                Console.WriteLine("Your estimated total for shipping this package is: $" + estimate);
+            }
+            Console.Read();
+        }
+    }
+}
